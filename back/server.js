a4476f088cb78014cb5b4a8116b357c5d6adf878
hyunyy1 <79568825+hyunyy1@users.noreply.github.com>
2023-02-04 17:51:46 +0900

    Update server.js

--- back/server.js
+++ back/server.js
@@ -16,7 +16,6 @@ const cors = require("cors");
 const path = require("path");
 var list = require("./routes/list");
 var add = require("./routes/add");
-var get = require('./routes/get');
 var search = require('./routes/search');
 
 
@@ -289,72 +288,8 @@ app.get('/logout',function(req, res) {
 
 
 
-// 술집 등록하기 (add)
-
-// 이미지 업로드
-const multer = require('multer');
-// const path = require('path');
-
-var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, "./public/images/");
-  },
-  filename: function(req, file, cb) {
-    cb(null, new Date().valueOf() + path.extname(file.originalname));
-  }
-});
-
-var upload = multer({storage: storage});
-
-
-// // db에 넣기
-app.post('/', upload.array('file'), async function(req, res) {
-
-  //-----------------현서 DB-----------------
-  const conn = await mysql.createConnection({
-    host : '127.0.0.1',
-    user : 'root',
-    password : 'hyunyy1@',
-    database : 'finaldb'
-  })
-
-  let addsql = `insert into Shop (user_id, shop_name, shop_addr, telno, latitude, longitude, shop_url, Field)
-  values (1, "${req.body.place_name}", "${req.body.place_address}", 
-  "${req.body.place_phone}", "${req.body.place_x}", "${req.body.place_y}", 
-  "${req.body.place_url}", "${req.body.place_text}")`
-
-  let re = await conn.query(addsql);  
-
-  
-//   // 카테고리 넣기 
-  let addsql2 = `SELECT LAST_INSERT_ID() AS last_id`;
-  let re2 = await conn.query(addsql2);
-  var getlast_id = re2[0].last_id;
-
-  var chkname = req.body.chk;
-  var chkarr = chkname.split(",");
-  var len = chkarr.length;
-
-  for(var i = 0; i < len; i++) {
-    let addsql3 = `insert into Shop_Category (shop_id, category_name) values ("${getlast_id}", "${chkarr[i]}")`
-    let re3 = await conn.query(addsql3);
-  }  
-  
-
-//   // 이미지 경로 넣기
-  var len2 = req.files.length;
-
-  for(var i = 0; i < len2; i++) {
-    let addsql4 = `insert into Shop_Img (shop_id, user_id, image) values ("${getlast_id}", 1, concat("/images/","${req.files[i].filename}"))`
-    let re4 = await conn.query(addsql4);
-  }
-})
-
-
-
 app.use("/list", list);
 app.use("/add", add);
-app.use('/get', get);
 app.use('/search', search)
 
 app.listen(8080, function () {
