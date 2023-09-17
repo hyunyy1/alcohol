8817552f416bfddb37eb9206aa15dfe8002a504b
hyunyy1 <79568825+hyunyy1@users.noreply.github.com>
2023-09-17 17:42:46 +0900

    Update add.js

--- back/routes/add.js
+++ back/routes/add.js
@@ -20,39 +20,21 @@ var storage = multer.diskStorage({
 
 var upload = multer({storage: storage});
 
-
 // db에 넣기
 router.post('/', upload.array('file'), async function(req, res) {
-
-  //-----------------현서 DB-----------------
   const conn = await mysql.createConnection({
     host : '127.0.0.1',
     user : 'root',
     password : 'hyunyy1@',
     database : 'finaldb'
-  })
-
-  //-----------------찬익님 DB-----------------
-  // const conn = await mysql.createConnection({
-  //   host : '127.0.0.1',
-  //   user : 'root',
-  //   password : 'piaoxin123',
-  //   database : 'node_project'
-  // })
+  });
   
-  //-----------------종욱 DB-----------------
-  // const conn = await mysql.createConnection({
-  //   host : 'localhost',
-  //   user : 'root',
-  //   password : '181015',
-  //   database : 'node_project'
-  // })
-
-  let sql = `insert into Shop (user_id, shop_name, shop_addr, telno, latitude, longitude, shop_url, Field)
+  try {
+    let sql = `insert into Shop (user_id, shop_name, shop_addr, telno, latitude, longitude, shop_url, Field)
   values (1, "${req.body.place_name}", "${req.body.place_address}", 
   "${req.body.place_phone}", "${req.body.place_x}", "${req.body.place_y}", 
   "${req.body.place_url}", "${req.body.place_text}")`
-
+    
   let re = await conn.query(sql);  
 
   // 카테고리 넣기 
@@ -69,7 +51,6 @@ router.post('/', upload.array('file'), async function(req, res) {
     let re3 = await conn.query(sql3);
   }  
   
-
   // 이미지 경로 넣기
   var len2 = req.files.length;
 
@@ -77,8 +58,9 @@ router.post('/', upload.array('file'), async function(req, res) {
     let sql4 = `insert into Shop_Img (shop_id, user_id, image) values ("${getlast_id}", 1, concat("/images/","${req.files[i].filename}"))`
     let re4 = await conn.query(sql4);
   }
+  } catch (err) {
+    console.error(err);
+  };
 })
-
-
 
 module.exports = router;
